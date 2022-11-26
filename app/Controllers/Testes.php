7b8096f7c0c2eeb966fccd5cbdbe49b382e339bf
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2022-11-26 10:07:18 -0300

    Adicionando elemento e propriedades a tag

--- app/Controllers/Testes.php
+++ app/Controllers/Testes.php
@@ -7,6 +7,13 @@ use Core\Tag;
 class Testes{
 
     public function index(){
-        (new Tag('h1'))->show();
+        $d = new Tag('div');
+        $h1 = new Tag('h1');
+        $h1->add('Joaquim Scavone');
+        $d->add($h1);
+        $h1->class = 'danger';
+        $d->add(new Tag('hr'));
+        $d->style = "background-color:red;";
+        $d->show();
     }
 }
--- app/core/Tag.php
+++ app/core/Tag.php
@@ -10,18 +10,60 @@ class Tag implements ViewElement{
     private $tagname;
     const VOID_ELEMENTS = array("area","base","br","col","embed","hr","img","input","keygen","link",
     "meta","param","source","track","wbr");
-
+    private $children = array();
+    private $properties = array();
+    private $void;
     public function __construct($tagname)
     {
         $this->tagname = $tagname;
+        $this->void = in_array($this->tagname,self::VOID_ELEMENTS);
+    }
+
+
+    public function add($element){
+        if($this->void){
+            throw new \Exception('Elementos Void não podem ter filhos!');
+        }
+        $this->children[] = $element;
+        return $this;
+    }
+
+    public function __set($name, $value){
+        $this->properties[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return (array_key_exists($name, $this->properties))
+            ? $this->properties[$name] : "";
     }
     
+    private function renderProperties(){
+        $prop = "";
+        foreach($this->properties as $key => $property){
+            $prop .= " {$key}='$property'";
+        }
+        return $prop;
+    }
+
     
     public function show(){
-        if(in_array($this->tagname,self::VOID_ELEMENTS)){
-            echo "<{$this->tagname}/>";
+        if($this->void){
+            echo "<{$this->tagname}{$this->renderProperties()}/>";
             return;
         }
-        echo "<{$this->tagname}></{$this->tagname}>";
+        echo "<{$this->tagname}{$this->renderProperties()}>";
+        foreach($this->children as $child){
+            if($child instanceof ViewElement){
+                $child->show();
+                continue;
+            }
+            echo $child;
+        }
+        echo "</{$this->tagname}>";
+    }
+
+    public function isVoid(){
+      return $this->void;
     }
 }
